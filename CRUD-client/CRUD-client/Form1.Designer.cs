2f8b1827a198b22f6278a7d41375c2971c04818d
andrearanica <andrearanica2004@gmail.com>
2023-01-26 16:34:10 +0100

    🎉 Start CRUD

---
+++ CRUD-client/CRUD-client/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace CRUD_client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_userId = new System.Windows.Forms.TextBox();
+            this.btn_saveUserId = new System.Windows.Forms.Button();
+            this.list_todos = new System.Windows.Forms.ListBox();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(227, 128);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Todo";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(191, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Inserisci il tuo userId";
+            // 
+            // txt_userId
+            // 
+            this.txt_userId.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_userId.Location = new System.Drawing.Point(147, 33);
+            this.txt_userId.Name = "txt_userId";
+            this.txt_userId.Size = new System.Drawing.Size(241, 29);
+            this.txt_userId.TabIndex = 3;
+            // 
+            // btn_saveUserId
+            // 
+            this.btn_saveUserId.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_saveUserId.Location = new System.Drawing.Point(234, 68);
+            this.btn_saveUserId.Name = "btn_saveUserId";
+            this.btn_saveUserId.Size = new System.Drawing.Size(75, 34);
+            this.btn_saveUserId.TabIndex = 4;
+            this.btn_saveUserId.Text = "Salva";
+            this.btn_saveUserId.UseVisualStyleBackColor = true;
+            this.btn_saveUserId.Click += new System.EventHandler(this.btn_saveUserId_Click);
+            // 
+            // list_todos
+            // 
+            this.list_todos.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.list_todos.FormattingEnabled = true;
+            this.list_todos.ItemHeight = 21;
+            this.list_todos.Location = new System.Drawing.Point(92, 187);
+            this.list_todos.Name = "list_todos";
+            this.list_todos.Size = new System.Drawing.Size(345, 172);
+            this.list_todos.TabIndex = 5;
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Location = new System.Drawing.Point(92, 365);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(90, 34);
+            this.btn_delete.TabIndex = 6;
+            this.btn_delete.Text = "Rimuovi";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 727);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.list_todos);
+            this.Controls.Add(this.btn_saveUserId);
+            this.Controls.Add(this.txt_userId);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Todo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_userId;
+        private System.Windows.Forms.Button btn_saveUserId;
+        private System.Windows.Forms.ListBox list_todos;
+        private System.Windows.Forms.Button btn_delete;
+    }
+}
+
---
+++ CRUD-client/CRUD-client/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace CRUD_client {
+    public partial class Form1 : Form {
+        int userId = 0;
+        static HttpClient client = new HttpClient();
+
+        static async Task<Todo> GetTodoAsync(string path)
+        {
+            Todo product = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                product =  await JsonSerializer.DeserializeAsync<Todo>(await response.Content.ReadAsStreamAsync());
+            }
+            return product;
+        }
+
+        static async Task<List<Todo>> GetTodoList (int userId) {
+            List<Todo> todos = null;
+            HttpResponseMessage response = await client.GetAsync($"/todos?userId={ userId }");
+            if (response.IsSuccessStatusCode) {
+                todos = await JsonSerializer.DeserializeAsync<List<Todo>>(await response.Content.ReadAsStreamAsync());
+            }
+            return todos;
+        }
+
+        static async Task<Uri> CreateTodoAsync(Todo todo)
+        {
+
+            HttpResponseMessage response = await client.PostAsJsonAsync(
+                "/todos", todo);
+            response.EnsureSuccessStatusCode();
+
+            // return URI of the created resource.
+            return response.Headers.Location;
+        }
+
+        static async Task<Todo> UpdateTodoAsync(Todo todo)
+        {
+            HttpResponseMessage response = await client.PutAsJsonAsync(
+                $"/todos/{ todo.id }", todo);
+            response.EnsureSuccessStatusCode();
+
+            // Deserialize the updated product from the response body.
+            todo = await JsonSerializer.DeserializeAsync<Todo>(await response.Content.ReadAsStreamAsync());
+            return todo;
+        }
+
+        static async Task<HttpStatusCode> DeleteTodoAsync(int id)
+        {
+            HttpResponseMessage response = await client.DeleteAsync(
+                $"/todos/{ id }");
+            return response.StatusCode;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void showTodos ()
+        {
+            try
+            {
+                list_todos.Items.Clear();
+                List<Todo> todos = await GetTodoList(userId);
+                foreach (Todo todo in todos) {
+                    if (todo.completed) { 
+                        list_todos.Items.Add($"{ todo.id } | ▢ { todo.title }");
+                    } else {
+                        list_todos.Items.Add($"▣ { todo.title }");
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        private void btn_saveUserId_Click(object sender, EventArgs e) {
+            userId = int.Parse(txt_userId.Text);
+            showTodos();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            client.BaseAddress = new Uri("http://localhost:8080/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        private async void btn_delete_Click(object sender, EventArgs e) {
+            int todoId = int.Parse(list_todos.Items[list_todos.SelectedIndex].ToString().Split(' ')[0]);
+            HttpStatusCode code = await DeleteTodoAsync(todoId);
+            if (code == HttpStatusCode.OK) {
+                MessageBox.Show("Eliminazione avvenuta con successo");
+                showTodos();
+            } else {
+                MessageBox.Show("Seleziona l'elemento da eliminare");
+            }
+        }
+    }
+    public class Todo
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public bool completed { get; set; }
+    }
+}
